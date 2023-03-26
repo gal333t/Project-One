2866e7525b5993599018d672896d11b6e88b9515
galit.moss <galitmoss@me.com>
2023-03-26 19:08:31 +1100

    fixed some small errors like adding let to for loop and removing the newGuess() from within click function

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const chosenWord = validWords[Math.floor(Math.random() * validWords.length)];
 
-// can't remember how Michael explained to do this so trying a different method
 let letterCounter = 0;
 let guessCounter = 0;
 
 let letters = [];
-// let letters = [div.letter, div.letter, div.letter, div.letter, div.letter];
 let guesses = [];
-// let guesses = [div.guess, div.guess, div.guess, div.guess, div.guess];
 
 let clickedLetter = [];
 
@@ -18,16 +15,12 @@ function onStartUp() {
 
 function newGuess() {
   letters = guesses[guessCounter].querySelectorAll(".letter");
-  // shows us by each box specifically of the first row
-  console.log(letters);
-  let i = letters;
-  for (i = 0; i < 5; i++) {
+
+  for (let i = 0; i < 5; i++) {
     // console.log(i);
     let p = document.createElement("p");
-    // add a ID with the name "letter" followed by the index for number
     p.setAttribute("id", `letter${[i]}`);
     p.textContent = clickedLetter[i];
-    console.log(clickedLetter[i]);
     let div = letters[i];
     div.appendChild(p);
   }
@@ -43,10 +36,7 @@ let keys = document.getElementsByClassName("key");
 for (let keyElement of keys) {
   let key = keyElement.textContent;
   keyElement.addEventListener("click", function () {
-    // take the key clicked, and type into a box
     console.log(key);
-    // whatever key is clicked, add to array 'clickedLetter'
     clickedLetter.push(key);
-    newGuess();
   });
 }
