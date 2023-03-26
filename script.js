899b353d8e57c34bc343e28f7c625d2aa723b65c
galit.moss <galitmoss@me.com>
2023-03-26 15:45:45 +1100

    cleaned up the page, made more changes to get newGuess to work better

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//
-
 const chosenWord = validWords[Math.floor(Math.random() * validWords.length)];
 
 // can't remember how Michael explained to do this so trying a different method
@@ -16,38 +14,26 @@ let clickedLetter = [];
 function onStartUp() {
   guesses = document.querySelectorAll(".guess");
   console.log(guesses);
-  // let i = guesses;
-  // for (i = 0; i < 6; i++) {
-  //   //
-  // }
-  newGuess();
 }
 
 function newGuess() {
   letters = guesses[guessCounter].querySelectorAll(".letter");
   // shows us by each box specifically of the first row
   console.log(letters);
-  // should i = letter counter?
   let i = letters;
   for (i = 0; i < 5; i++) {
-    console.log(i);
+    // console.log(i);
     let p = document.createElement("p");
     // add a ID with the name "letter" followed by the index for number
     p.setAttribute("id", `letter${[i]}`);
-    // will update when keyboard works to input a letter based on it's index in the word
     p.textContent = clickedLetter[i];
-    // finds the div based on it's index again going off index in that loop
+    console.log(clickedLetter[i]);
     let div = letters[i];
-    console.log(p);
     div.appendChild(p);
   }
 }
 
 onStartUp();
-
-// function onReturn() {
-// on return key
-// }
 
 // keyboard
 
@@ -61,23 +47,6 @@ for (let keyElement of keys) {
     console.log(key);
     // whatever key is clicked, add to array 'clickedLetter'
     clickedLetter.push(key);
+    newGuess();
   });
 }
-
-// for (let keyElement of keys) {
-//   let key = keyElement.textContent;
-//   keyElement.addEventListener("click", function () {
-//     // take the key clicked, and type into a box
-//     console.log(key);
-
-//     if (key === "Backspace") {
-//       let output = document.querySelectorAll("p");
-//       output.textContent = "";
-//     } else {
-//       let div = document.querySelector(".letter");
-//       let letter0 = document.createElement("p");
-//       letter0.textContent = key;
-//       div.appendChild(letter0);
-//     }
-//   });
-// }
