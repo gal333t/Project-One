59b703785b08edb4e6856e59399873e2c26b0aa4
galit.moss <galitmoss@me.com>
2023-03-27 20:59:50 +1100

    added checkLetters function and new array to split chosenWord

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const chosenWord = validWords[Math.floor(Math.random() * validWords.length)];
+let chosenWordArr = chosenWord.split("");
+// this will split the word into letters
 
 let letterCounter = 0;
 let guessCounter = 0;
@@ -13,18 +15,39 @@ function onStartUp() {
   console.log(guesses);
 }
 
-// this now works on return key click and puts the letters in the row correctly
-// still want to run it to input each letter as you type
-
 function onReturn() {
   letters = guesses[guessCounter].querySelectorAll(".letter");
-  for (let i = 0; i < 5; i++) {
-    // console.log(i);
-    let p = document.createElement("p");
-    p.setAttribute("id", `letter${[i]}`);
-    p.textContent = clickedLetter[i];
-    let div = letters[i];
-    div.appendChild(p);
+
+  if (clickedLetter.length != 5) {
+    alert("invalid word");
+  } else {
+    for (let i = 0; i < 5; i++) {
+      // console.log(i);
+      let p = document.createElement("p");
+      p.setAttribute("id", `letter${[i]}`);
+      p.textContent = clickedLetter[i];
+      let div = letters[i];
+      div.appendChild(p);
+      checkWord();
+    }
+  }
+}
+
+function checkWord() {
+  if (validWords.includes(clickedLetter.join("")) == false) {
+    console.log("Word is not valid");
+  }
+}
+
+// checkLetters() was pretty much copied from Geeks for Geeks website, adjusted to make it work for what I wanted
+
+function checkLetters(chosenWordArr, clickedLetter) {
+  for (let i = 0; i < chosenWordArr.length; i++) {
+    for (let x = 0; x < clickedLetter.length; x++) {
+      if (chosenWordArr[i] === clickedLetter[x]) {
+        console.log(clickedLetter[x]);
+      }
+    }
   }
 }
 
